6177696a83bdea5ba6f70092c0ab63dffddbbd3d
lafaro4 <adri.alfaro.luis@gmail.com>
2020-06-16 20:42:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save(function (err) {
+        if (!err) {
+            res.render("secrets");
+        } else {
+            res.render(err);
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: userName}, function (err, foundUser) {
+        if (!err) {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
+app.listen(3000, function () {
+
+    console.log("Successfully started server");
+});
